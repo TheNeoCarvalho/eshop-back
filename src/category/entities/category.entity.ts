faafa9c4f6705c0e9f4f788e2edec49d3a7510d2
Manoel Carvalho <manoelmecat@gmail.com>
2023-05-30 15:40:42 -0300

    feat: create category and product

---
+++ src/category/entities/category.entity.ts
@@ -0,0 +1,20 @@
+import { ProductEntity } from 'src/product/entities/product.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'categoty' })
+export class CategoryEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id?: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'created_at' })
+  createdAt: Date;
+
+  @Column({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany(() => ProductEntity, (product: ProductEntity) => product.category)
+  products?: ProductEntity;
+}
